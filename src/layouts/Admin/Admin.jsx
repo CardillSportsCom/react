9b81bc9585abd03003da2cac44e2b4398bcc47ba
vithushan <vithushan19@gmail.com>
2020-02-28 11:31:47 -0500

    remove footer

--- src/layouts/Admin/Admin.jsx
+++ src/layouts/Admin/Admin.jsx
@@ -205,11 +205,7 @@ class Admin extends React.Component {
             toggleSidebar={this.toggleSidebar}
           />
           <Switch>{this.getRoutes(routes)}</Switch>
-          {// we don't want the Footer to be rendered on full screen maps page
-          this.props.location.pathname.indexOf("full-screen-map") !==
-          -1 ? null : (
-            <Footer fluid />
-          )}
+     
         </div>
 
       </div>
