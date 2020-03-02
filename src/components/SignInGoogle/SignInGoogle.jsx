ae959fd9c67ee43e9e2f97e9ee193ffbbb574a23
vithushan <vithushan19@gmail.com>
2020-03-01 21:29:18 -0500

    call auth after sign in

--- src/components/SignInGoogle/SignInGoogle.jsx
+++ src/components/SignInGoogle/SignInGoogle.jsx
@@ -2,19 +2,39 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
 import { withFirebase } from "../Firebase";
-import { Card, CardHeader, CardBody } from "reactstrap";
 
 class SignInGoogleBase extends Component {
   constructor(props) {
     super(props);
     this.state = { error: null };
   }
+
   onSubmit = event => {
+    var that = this;
+    const API_URL = "https://api-cardillsports-st.herokuapp.com";
     this.props.firebase
       .doSignInWithGoogle()
       .then(() => {
         this.setState({ error: null });
-        this.props.history.push("/admin/dashboard/");
+        this.props.firebase.auth.currentUser.getIdToken().then(function(idToken) {
+          // <------ Get firebase token
+          const opts = {
+            token: idToken
+          };
+          fetch(API_URL + "/auth", {
+            method: "post",
+            body: JSON.stringify(opts)
+          }).then(function(response) {
+            response.json().then(data => {
+              localStorage.setItem("idToken", data["id_token"]);
+              localStorage.setItem("playerId", data.player.id);
+              localStorage.setItem("playerImage", data.player.imageUri);
+              that.props.history.push("/admin/dashboard/");
+            });
+
+            return "";
+          });
+        });
       })
       .catch(error => {
         this.setState({ error });
@@ -26,7 +46,10 @@ class SignInGoogleBase extends Component {
     return (
       <div className="card mb-3">
         <div className="card bg-dark text-white">
-          <img alt="cardill header" src={require("./../../assets/img/header.png")} />
+          <img
+            alt="cardill header"
+            src={require("./../../assets/img/header.png")}
+          />
           <div className="card-img-overlay center">
             <form onSubmit={this.onSubmit}>
               <button type="submit">
